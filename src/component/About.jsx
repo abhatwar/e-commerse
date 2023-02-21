845e31bd609669bc1fa12c6faeae6d1300916b22
abhatwar <108324438+abhatwar@users.noreply.github.com>
2023-02-21 11:32:20 +0530

    code

---
+++ src/component/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="px-4 my-5 bg-light rounded-3 py-5">
+        <div className="container py-4">
+          <div className="row">
+            <h3>Company is around a globe since 2012.</h3>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default About
